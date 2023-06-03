b7b32f00e3abefa27a87eca03c5f6a7675b82ff3
pavle99 <mi18024@alas.matf.bg.ac.rs>
2023-06-03 20:04:36 +0200

    define colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        tomato: "#ff5a5a",
+        success: "#51DA88",
+        error: "#DA5151",
+        warning: "#F5AF45",
+        info: "#51B1DA",
+        glass: "rgba(23, 23, 22, 0.5)",
       },
     },
   },
   plugins: [],
-}
+};
