b614221b4f38991c41e8fc202dd2a0a0801687f8
pavle99 <mi18024@alas.matf.bg.ac.rs>
2023-06-20 19:35:21 +0200

    padding fixes

--- src/app/(authed)/home/components/create-room.tsx
+++ src/app/(authed)/home/components/create-room.tsx
@@ -60,7 +60,7 @@ const CreateRoom = () => {
           <div
             key={dungeon._id}
             className={cn(
-              "cursor-pointer flex flex-row gap-8 hover:bg-white/5 rounded-md p-4 transition-all duration-300",
+              "cursor-pointer flex flex-row gap-8 hover:bg-white/5 rounded-md p-4 pr-0 transition-all duration-300",
               dungeon === selectedDungeon && "bg-white/5",
             )}
             onClick={() => setSelectedDungeon(selectedDungeon === dungeon ? undefined : dungeon)}
