9cc86f5cd7e66df9e403c19f384f1ef8e7c45062
vita-ride <viktornovakovic77@gmail.com>
2023-06-22 20:04:45 +0200

    Remove needless fetch

--- src/app/(authed)/room/[conversationId]/components/player.tsx
+++ src/app/(authed)/room/[conversationId]/components/player.tsx
@@ -3,20 +3,15 @@ import { IPlayer } from "@/types/dnd";
 import Image from "next/image";
 
 const Player = ({ player }: { player: IPlayer }) => {
-  const { data: avatarData, isLoading } = useGetAvatar(player.avatarId);
-
   return (
     <div key={player.accountId} className="flex flex-row gap-6">
-      {!isLoading && (
-        <Image
-          src={avatarData?.imageUrl || "/images/default-avatar.png"}
-          width={64}
-          height={64}
-          alt={`player-${player.accountId}-avatar`}
-          className="w-16 h-16"
-        />
-      )}
-      {isLoading && <div className="h-16 w-16" />}
+      <Image
+        src={player.avatarImageUrl || "/images/default-avatar.png"}
+        width={64}
+        height={64}
+        alt={`player-${player.accountId}-avatar`}
+        className="w-16 h-16"
+      />
 
       <div className="flex flex-col gap-1">
         <p className="text-2xl">{player.name}</p>
