34f29011d81b8d0fda8a5a048b668528e7181be0
pavle99 <mi18024@alas.matf.bg.ac.rs>
2023-07-01 20:01:37 +0200

    logo fix 2

--- src/components/mobile-navbar.tsx
+++ src/components/mobile-navbar.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import { AiOutlineLeft } from "react-icons/ai";
-import mobileNavbarLogo from "../../public/images/mobile-navbar-logo.svg";
 
 const MobileNavbar = () => {
   return (
     <div className="flex w-full justify-between  px-5 md:hidden">
-      <Image src={mobileNavbarLogo} alt="mobile-navbar-logo" />
+      <Image src="/images/mobile-navbar-logo.svg" width={36} height={24} alt="mobile-navbar-logo" />
       <Link
         className="flex gap-1 items-center font-medium leading-3 tracking-wider uppercase"
         href="/home"
