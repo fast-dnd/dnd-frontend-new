0a4d0de356efd8a90a98398684ea410d5dbba883
pavle99 <mi18024@alas.matf.bg.ac.rs>
2023-07-14 13:10:04 +0200

    refactoring dungeon create location and champion

--- src/app/(authed)/create-dungeon/[[...dungeonId]]/components/champion-location-wrapper.tsx
+++ src/app/(authed)/create-dungeon/[[...dungeonId]]/components/champion-location-wrapper.tsx
@@ -1,43 +1,99 @@
 import React from "react";
 import { DevTool } from "@hookform/devtools";
-import { Control } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { produce } from "immer";
+import { FieldErrors, SubmitHandler, useForm, UseFormRegister } from "react-hook-form";
 
+import useStore from "@/hooks/use-store";
 import { Button } from "@/components/ui/button";
 
-import { StatusType } from "../stores/form-store";
+import { championSchema, IChampionSchema } from "../schemas/champion-schema";
+import { ILocationSchema, locationSchema } from "../schemas/location-schema";
+import { StatusType, useDungeonFormStore } from "../stores/form-store";
 
-interface IChampionLocationWrapperProps {
-  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-  onCancel: () => void;
+export interface IChampionLocationProps {
   status: StatusType;
-  title: string;
-  description: string;
-  submitButtonText: string;
-  formId: string;
-  children: React.ReactNode;
+  setStatus: React.Dispatch<React.SetStateAction<StatusType>>;
+  editIndex: number;
+  setEditIndex: React.Dispatch<React.SetStateAction<number>>;
+}
+interface IChampionLocationWrapperProps extends IChampionLocationProps {
+  description?: string;
   locationOrChampion: "Location" | "Champion";
-  control: Control<any>;
+  children: React.ReactNode | ((props: IChildrenProps) => React.ReactNode);
+}
+
+interface IChildrenProps {
+  register: UseFormRegister<ILocationSchema | IChampionSchema>;
+  errors: FieldErrors<ILocationSchema | IChampionSchema>;
 }
 
 const ChampionLocationWrapper = ({
-  onSubmit,
-  onCancel,
-  status,
   description,
-  formId,
   children,
   locationOrChampion,
-  control,
+  status,
+  setStatus,
+  editIndex,
+  setEditIndex,
 }: IChampionLocationWrapperProps) => {
+  const dungeonFormStore = useStore(useDungeonFormStore, (state) => state);
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    control,
+    formState: { errors },
+  } = useForm<IChampionSchema | ILocationSchema>({
+    resolver: zodResolver(locationOrChampion === "Champion" ? championSchema : locationSchema),
+    values:
+      status === "EDITING" ? dungeonFormStore?.dungeonFormData.champions[editIndex] : undefined,
+  });
+
+  if (!dungeonFormStore) return null;
+
+  const { dungeonFormData, updateDungeonFormData } = dungeonFormStore;
+
+  const onSubmit: SubmitHandler<ILocationSchema | IChampionSchema> = (data) => {
+    const updateField = locationOrChampion === "Location" ? "locations" : "champions";
+    if (status === "CREATING") {
+      updateDungeonFormData(
+        produce(dungeonFormData, (draft) => {
+          draft[updateField].push(data as any);
+        }),
+      );
+    } else if (status === "EDITING") {
+      updateDungeonFormData(
+        produce(dungeonFormData, (draft) => {
+          draft[updateField][editIndex] = data;
+        }),
+      );
+    }
+
+    setStatus("LIST");
+    setEditIndex(-1);
+  };
+
+  const onCancel = () => {
+    reset();
+    setStatus("LIST");
+    setEditIndex(-1);
+  };
+
   return (
-    <form className="flex h-full flex-col gap-8 lg:w-full" onSubmit={onSubmit}>
+    <form className="flex h-full flex-col gap-8 lg:w-full" onSubmit={handleSubmit(onSubmit)}>
       <p className="-my-1 text-xl font-semibold uppercase tracking-[0.07em] text-white/50">
         {status === "CREATING" ? `Create ${locationOrChampion}` : `Edit ${locationOrChampion}`}
       </p>
       {description && (
         <p className="-my-1 text-lg tracking-[0.07em] text-white/50">{description}</p>
       )}
-      <div className="flex h-full flex-col gap-5 lg:flex-row lg:gap-8">{children}</div>
+
+      <div className="flex h-full flex-col gap-5 lg:flex-row lg:gap-8">
+        {typeof children === "function" ? children({ register, errors }) : children}
+      </div>
+
       <div className="flex flex-row justify-between gap-8 lg:justify-end">
         <Button
           className="w-fit text-sm lg:text-lg"
@@ -54,7 +110,7 @@ const ChampionLocationWrapper = ({
           {status === "CREATING" ? `ADD ${locationOrChampion}` : `EDIT ${locationOrChampion}`}
         </Button>
       </div>
-      <DevTool control={control} id={formId} />
+      <DevTool control={control} id={`${locationOrChampion}-${editIndex}-form`} />
     </form>
   );
 };
--- src/app/(authed)/create-dungeon/[[...dungeonId]]/components/location.tsx
+++ src/app/(authed)/create-dungeon/[[...dungeonId]]/components/location.tsx
@@ -1,18 +1,13 @@
 "use client";
 
 import { Dispatch, SetStateAction } from "react";
-import { DevTool } from "@hookform/devtools";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { produce } from "immer";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { FieldErrors } from "react-hook-form";
 
-import useStore from "@/hooks/use-store";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { TextArea } from "@/components/ui/text-area";
 
-import { ILocationSchema, locationSchema } from "../schemas/location-schema";
-import { useDungeonFormStore } from "../stores/form-store";
+import { ILocationSchema } from "../schemas/location-schema";
+import ChampionLocationWrapper from "./champion-location-wrapper";
 
 interface ILocationProps {
   status: "LIST" | "CREATING" | "EDITING";
@@ -22,111 +17,63 @@ interface ILocationProps {
 }
 
 const Location = ({ status, setStatus, editIndex, setEditIndex }: ILocationProps) => {
-  const dungeonFormStore = useStore(useDungeonFormStore, (state) => state);
-
-  const {
-    register,
-    handleSubmit,
-    reset,
-    control,
-    formState: { errors },
-  } = useForm<ILocationSchema>({
-    resolver: zodResolver(locationSchema),
-    values:
-      status === "EDITING" ? dungeonFormStore?.dungeonFormData.locations[editIndex] : undefined,
-  });
-
-  if (!dungeonFormStore) return null;
-
-  const { dungeonFormData, updateDungeonFormData } = dungeonFormStore;
-
-  const onSubmit: SubmitHandler<ILocationSchema> = (data) => {
-    if (status === "CREATING") {
-      updateDungeonFormData(
-        produce(dungeonFormData, (draft) => {
-          draft.locations.push(data);
-        }),
-      );
-    } else if (status === "EDITING") {
-      updateDungeonFormData(
-        produce(dungeonFormData, (draft) => {
-          draft.locations[editIndex] = data;
-        }),
-      );
-    }
-
-    setStatus("LIST");
-    setEditIndex(-1);
-  };
-
-  const onCancel = () => {
-    reset();
-    setStatus("LIST");
-    setEditIndex(-1);
-  };
-
   return (
-    <form className="flex h-full flex-col gap-8 lg:w-full" onSubmit={handleSubmit(onSubmit)}>
-      <p className="-my-1 text-xl font-semibold uppercase tracking-[0.07em] text-white/50">
-        {status === "CREATING" ? "Add Location" : "Edit Location"}
-      </p>
-      <div className="flex h-full flex-col gap-5 lg:flex-row lg:gap-8">
-        <div className="flex min-h-0 flex-col gap-5 lg:h-full lg:basis-1/3 lg:gap-8">
-          <Input
-            label="Name"
-            placeholder="Misty Forest"
-            className="m-0"
-            {...register("name")}
-            state={errors?.name ? "error" : undefined}
-            errorMessage={errors?.name?.message}
-          />
-          <div className="flex min-h-0 lg:h-full">
-            <TextArea
-              label="Mission"
-              placeholder="The goal is to find the secret treasure hidden somewhere in the old castle..."
-              className="m-0 h-full"
-              {...register("mission")}
-              state={errors?.mission ? "error" : undefined}
-              errorMessage={errors?.mission?.message}
-            />
-          </div>
-        </div>
-        <div className="flex min-h-0 lg:h-full lg:basis-1/3">
-          <TextArea
-            label="Description"
-            placeholder="Venture into the heart of an enchanted forest..."
-            className="m-0 h-full"
-            {...register("description")}
-            state={errors?.description ? "error" : undefined}
-            errorMessage={errors?.description?.message}
-          />
-        </div>
-        <div className="flex min-h-0 lg:h-full lg:basis-1/3">
-          <TextArea
-            label="Transition"
-            placeholder="You leave this place to enter the dark swamp..."
-            className="m-0 h-full"
-            {...register("transition")}
-            state={errors?.transition ? "error" : undefined}
-            errorMessage={errors?.transition?.message}
-          />
-        </div>
-      </div>
-      <div className="flex flex-row justify-between gap-8 lg:justify-end">
-        <Button
-          className="w-fit text-sm lg:text-lg"
-          variant="ghost"
-          onClick={onCancel}
-          type="reset"
-        >
-          CANCEL
-        </Button>
-        <Button className="w-fit whitespace-nowrap px-8 text-sm lg:text-lg" variant="outline">
-          {status === "CREATING" ? "ADD LOCATION" : "EDIT LOCATION"}
-        </Button>
-      </div>
-      <DevTool control={control} id={`location-${editIndex}-form`} />
-    </form>
+    <ChampionLocationWrapper
+      status={status}
+      setStatus={setStatus}
+      editIndex={editIndex}
+      setEditIndex={setEditIndex}
+      locationOrChampion="Location"
+    >
+      {({ register, errors }) => {
+        const locationErrors = errors as FieldErrors<ILocationSchema>;
+
+        return (
+          <>
+            <div className="flex min-h-0 flex-col gap-5 lg:h-full lg:basis-1/3 lg:gap-8">
+              <Input
+                label="Name"
+                placeholder="Misty Forest"
+                className="m-0"
+                {...register("name")}
+                state={locationErrors?.name ? "error" : undefined}
+                errorMessage={locationErrors?.name?.message}
+              />
+              <div className="flex min-h-0 lg:h-full">
+                <TextArea
+                  label="Mission"
+                  placeholder="The goal is to find the secret treasure hidden somewhere in the old castle..."
+                  className="m-0 h-full"
+                  {...register("mission")}
+                  state={locationErrors?.mission ? "error" : undefined}
+                  errorMessage={locationErrors?.mission?.message}
+                />
+              </div>
+            </div>
+            <div className="flex min-h-0 lg:h-full lg:basis-1/3">
+              <TextArea
+                label="Description"
+                placeholder="Venture into the heart of an enchanted forest..."
+                className="m-0 h-full"
+                {...register("description")}
+                state={locationErrors?.description ? "error" : undefined}
+                errorMessage={locationErrors?.description?.message}
+              />
+            </div>
+            <div className="flex min-h-0 lg:h-full lg:basis-1/3">
+              <TextArea
+                label="Transition"
+                placeholder="You leave this place to enter the dark swamp..."
+                className="m-0 h-full"
+                {...register("transition")}
+                state={locationErrors?.transition ? "error" : undefined}
+                errorMessage={locationErrors?.transition?.message}
+              />
+            </div>
+          </>
+        );
+      }}
+    </ChampionLocationWrapper>
   );
 };
 
