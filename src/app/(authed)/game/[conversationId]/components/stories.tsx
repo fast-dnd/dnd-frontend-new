befde80a4d075937826b4a7b080e8302678a6c73
pavle99 <mi18024@alas.matf.bg.ac.rs>
2023-07-24 14:05:21 +0200

    typecheck for generated image

--- src/app/(authed)/game/[conversationId]/components/stories.tsx
+++ src/app/(authed)/game/[conversationId]/components/stories.tsx
@@ -37,48 +37,48 @@ const Stories = ({ roomData, dungeonData, lastStory }: StoriesProps) => {
 
   return (
     <div className="flex  w-full flex-1 flex-col gap-8 pr-4 lg:max-h-full lg:overflow-y-auto lg:pr-6">
-      {stories.map((story, i) => (
-        <div key={i} className="flex w-full flex-col gap-8">
-          <div className="flex w-full items-center gap-8">
-            <div className="flex flex-col text-lg font-semibold uppercase tracking-[0.2em] lg:flex-row lg:text-2xl">
-              <span className="mr-2 text-tomato">
-                TURN {i + 1}/{roomData.maxRounds + 1}.
-              </span>
-              <span>{dungeonData.locations[Math.floor(i / 2)]?.name}</span>
-            </div>
-            <div className="border-t border-tomato lg:flex-1" />
-          </div>
-          <div className="w-full">
-            {roomData.generateImages && i % 2 === 0 && (
-              <div className="mb-4 flex aspect-square w-full shrink-0 justify-center lg:float-left lg:mr-6 lg:inline-block lg:h-72 lg:w-72">
-                {roomData.generatedImages[i] && roomData.generatedImages[i].length > 0 ? (
-                  <>
-                    <ImageModal image={roomData.generatedImages[i]} className="hidden lg:block" />
-                    <ImageModal
-                      image={roomData.generatedImages[i]}
-                      isMobile={true}
-                      className="lg:hidden"
-                    />
-                  </>
-                ) : (
-                  <div className="flex h-full w-full animate-pulse items-center justify-center rounded bg-gray-600">
-                    <SkeletonIcon className="h-24 w-24 text-gray-200" />
-                  </div>
-                )}
-              </div>
-            )}
+      {stories.map((story, i) => {
+        const generatedImage = roomData.generatedImages[i];
 
-            <div className="tracking-widest lg:text-[22px] lg:leading-8">
-              {roomData.generateAudio && (
-                <div className="mb-4">
-                  <StyledAudio audio={roomData.generatedAudio[i]} />
+        return (
+          <div key={i} className="flex w-full flex-col gap-8">
+            <div className="flex w-full items-center gap-8">
+              <div className="flex flex-col text-lg font-semibold uppercase tracking-[0.2em] lg:flex-row lg:text-2xl">
+                <span className="mr-2 text-tomato">
+                  TURN {i + 1}/{roomData.maxRounds + 1}.
+                </span>
+                <span>{dungeonData.locations[Math.floor(i / 2)]?.name}</span>
+              </div>
+              <div className="border-t border-tomato lg:flex-1" />
+            </div>
+            <div className="w-full">
+              {roomData.generateImages && i % 2 === 0 && (
+                <div className="mb-4 flex aspect-square w-full shrink-0 justify-center lg:float-left lg:mr-6 lg:inline-block lg:h-72 lg:w-72">
+                  {generatedImage && generatedImage.length > 0 ? (
+                    <>
+                      <ImageModal image={generatedImage} className="hidden lg:block" />
+                      <ImageModal image={generatedImage} isMobile={true} className="lg:hidden" />
+                    </>
+                  ) : (
+                    <div className="flex h-full w-full animate-pulse items-center justify-center rounded bg-gray-600">
+                      <SkeletonIcon className="h-24 w-24 text-gray-200" />
+                    </div>
+                  )}
                 </div>
               )}
-              {story}
+
+              <div className="tracking-widest lg:text-[22px] lg:leading-8">
+                {roomData.generateAudio && (
+                  <div className="mb-4">
+                    <StyledAudio audio={roomData.generatedAudio[i]} />
+                  </div>
+                )}
+                {story}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
       <div ref={autoBottomScrollDiv} />
     </div>
   );
