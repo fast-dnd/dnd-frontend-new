ac1c2febefbe1b362b27d69b3ca052e47dfaec1a
pavle99 <mi18024@alas.matf.bg.ac.rs>
2023-10-03 10:30:30 +0200

    navbar fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -2,14 +2,17 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import checkJWT from "@/utils/check-jwt";
+import { cn } from "@/utils/style-utils";
 import useGetAccount from "@/hooks/use-get-account";
 
 import Coin from "./coin";
 
 const Navbar = ({ authed = true }: { authed?: boolean }) => {
   const loggedIn = authed || checkJWT();
+  const pathname = usePathname();
 
   const { data: account } = useGetAccount(!loggedIn);
 
@@ -17,12 +20,31 @@ const Navbar = ({ authed = true }: { authed?: boolean }) => {
     <div className="hidden w-full items-center justify-between gap-12 pl-2 pr-8 lg:flex">
       <Image src="/images/logo.png" width={300} height={100} alt="logo" />
       <div className="flex items-center gap-6 text-2xl leading-7 tracking-[3.3px]">
-        <Link href={authed || account ? "/home" : "/login"}>
+        <Link
+          href={authed || account ? "/home" : "/login"}
+          className={cn(pathname === "/home" && "border-b-4 border-primary-500")}
+        >
           {authed || account ? "PLAY" : "LOG IN"}
         </Link>
         <div className="h-2 w-2 rotate-45 bg-white opacity-25" />
-        <Link href="/guide">HOW TO PLAY</Link>
+        <Link
+          href="/guide"
+          className={cn(pathname === "/guide" && "border-b-4 border-primary-500")}
+        >
+          HOW TO PLAY
+        </Link>
         <div className="h-2 w-2 rotate-45 bg-white opacity-25" />
+        <div className="flex gap-6 rounded-md bg-white/10 px-4 py-3 backdrop-blur-sm">
+          <div className="flex items-center gap-1">
+            <Coin silver />
+            {account?.account.coins ?? "-"}
+          </div>
+          <div className="flex items-center gap-1">
+            <Coin />
+
+            {account?.account.dmCurrency ?? "-"}
+          </div>
+        </div>
         {loggedIn && (
           <>
             <Link href="/profile">
@@ -34,18 +56,6 @@ const Navbar = ({ authed = true }: { authed?: boolean }) => {
                 className="rounded-md"
               />
             </Link>
-
-            <div className="flex gap-6 rounded-md bg-white/10 px-4 py-3 backdrop-blur-sm">
-              <div className="flex items-center gap-1">
-                <Coin silver />
-                {account?.account.coins ?? "-"}
-              </div>
-              <div className="flex items-center gap-1">
-                <Coin />
-
-                {account?.account.dmCurrency ?? "-"}
-              </div>
-            </div>
           </>
         )}
       </div>
