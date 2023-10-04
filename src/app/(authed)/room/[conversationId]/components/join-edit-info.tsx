f3f966ceb5e254247248641651ec424262f8d4c8
vita-ride <viktornovakovic77@gmail.com>
2023-10-04 13:07:42 +0200

    Fix sizing in profile and lobby

--- src/app/(authed)/room/[conversationId]/components/join-edit-info.tsx
+++ src/app/(authed)/room/[conversationId]/components/join-edit-info.tsx
@@ -27,7 +27,7 @@ const JoinEditInfo = (props: { conversationId: string }) => {
       <Box
         title=""
         className="mb-4 flex h-full min-h-0 flex-1 flex-col items-center justify-center rounded-t-md p-8 lg:mb-0 lg:gap-8"
-        wrapperClassName="max-h-[745px] basis-1/4"
+        wrapperClassName="h-full basis-1/4"
         titleClassName="hidden"
       >
         <Spinner className="h-40 w-40" />
@@ -40,28 +40,27 @@ const JoinEditInfo = (props: { conversationId: string }) => {
     <Box
       title=""
       className="mb-4 flex h-full min-h-0 flex-1 flex-col justify-between gap-5 overflow-y-auto rounded-t-md p-8 text-sm lg:mb-0 lg:gap-8"
-      wrapperClassName="max-h-[745px] basis-1/4"
+      wrapperClassName="h-full basis-1/4"
       titleClassName="hidden"
     >
-      <div className="flex flex-col gap-6">
+      <div className="flex min-h-[150px] flex-1 flex-col gap-6">
         <p className="text-lg font-semibold uppercase">PLAYERS</p>
-        <div className="flex flex-col gap-4">
+        <div className="flex min-h-0 flex-1 flex-col gap-4 overflow-y-auto">
           {roomData.playerState.map((player) => (
             <Player key={player.accountId} player={player} />
           ))}
         </div>
-        <div className="flex flex-col justify-between gap-4 text-center lg:flex-row">
-          <p className="mt-2 flex-1 whitespace-nowrap text-xl">{roomData.link}</p>
-          <Button
-            onClick={() => onCopy(roomData.link)}
-            variant={copied ? "primary" : "outline"}
-            className="w-full flex-1 whitespace-nowrap px-8 text-lg uppercase lg:w-fit"
-          >
-            {copied ? "Copied" : "Copy ID"}
-          </Button>
-        </div>
       </div>
-
+      <div className="flex flex-col justify-between gap-4 text-center lg:flex-row">
+        <p className="mt-2 flex-1 whitespace-nowrap text-xl">{roomData.link}</p>
+        <Button
+          onClick={() => onCopy(roomData.link)}
+          variant={copied ? "primary" : "outline"}
+          className="w-full flex-1 whitespace-nowrap px-8 text-lg uppercase lg:w-fit"
+        >
+          {copied ? "Copied" : "Copy ID"}
+        </Button>
+      </div>
       <UpdateRoom conversationId={conversationId} roomData={roomData} dungeonData={dungeonData} />
     </Box>
   );
