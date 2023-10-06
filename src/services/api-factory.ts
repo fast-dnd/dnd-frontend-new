a232412295086dfb29de39efc73bba79dbfe63b2
pavle99 <mi18024@alas.matf.bg.ac.rs>
2023-10-06 14:32:26 +0200

    fix backend url

--- src/services/api-factory.ts
+++ src/services/api-factory.ts
@@ -55,8 +55,8 @@ const createApi = (options: IApiOptions) => {
 
 export default createApi;
 
-export const BACKEND_URL = "http://192.168.0.17:8080/";
+// export const BACKEND_URL = "http://192.168.0.17:8080/";
 
-// export const BACKEND_URL = "https://api-dev.v3rpg.com/";
+export const BACKEND_URL = "https://api-dev.v3rpg.com/";
 
 export const PAGINATION_LIMIT = 5;
