810a816803a407fde3cf4788ee0f668ec71be9b6
vita-ride <viktornovakovic77@gmail.com>
2023-10-17 22:23:08 +0200

    Fix

--- src/components/cookie-banner.tsx
+++ src/components/cookie-banner.tsx
@@ -68,36 +68,4 @@ export default function CookieBanner() {
       </div>
     </div>
   );
-
-  return (
-    <div
-      className={cn(
-        "inset-x-0 bottom-0 flex w-full items-center justify-between gap-4 rounded-lg bg-gray-700 p-3 shadow sm:flex-row md:max-w-screen-sm md:px-4",
-        cookieConsent !== null ? "hidden" : "flex",
-      )}
-    >
-      <div className="text-center">
-        <Link href="/info/cookies">
-          <p>
-            We use <span className="font-bold text-sky-400">cookies</span> on our site.
-          </p>
-        </Link>
-      </div>
-
-      <div className="flex gap-2">
-        <button
-          className="rounded-md border-gray-900 px-5 py-2 text-gray-300"
-          onClick={() => setCookieConsent(false)}
-        >
-          Decline
-        </button>
-        <button
-          className="rounded-lg bg-gray-900 px-5 py-2 text-white"
-          onClick={() => setCookieConsent(true)}
-        >
-          Allow Cookies
-        </button>
-      </div>
-    </div>
-  );
 }
