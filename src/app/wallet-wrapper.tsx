56f23c068a0ce88be82159fb4aa43e4985643c83
marsic3 <arsic.milos30@gmail.com>
2023-10-29 14:54:03 +0100

    switch to pnpm

--- src/app/wallet-wrapper.tsx
+++ src/app/wallet-wrapper.tsx
@@ -8,8 +8,6 @@ import { PhantomWalletAdapter, SolflareWalletAdapter } from "@solana/wallet-adap
 
 import "@solana/wallet-adapter-react-ui/styles.css";
 
-import { BackpackWalletAdapter } from "@solana/wallet-adapter-backpack";
-
 import { SOLANA_RPC_HOST_DEVNET } from "@/utils/constants";
 
 export default function CustomWalletWrapper({ children }: React.PropsWithChildren) {
@@ -22,7 +20,6 @@ export default function CustomWalletWrapper({ children }: React.PropsWithChildre
     () => [
       new PhantomWalletAdapter(),
       new SolflareWalletAdapter({ network }),
-      new BackpackWalletAdapter({ network }),
     ],
     [network],
   );
