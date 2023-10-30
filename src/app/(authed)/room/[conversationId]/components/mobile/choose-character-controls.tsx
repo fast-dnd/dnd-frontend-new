f947c7b67cfb253966c888671ad10899f7a21d89
vita-ride <viktornovakovic77@gmail.com>
2023-10-30 21:07:22 +0100

    Fix character controls positioning

--- src/app/(authed)/room/[conversationId]/components/mobile/choose-character-controls.tsx
+++ src/app/(authed)/room/[conversationId]/components/mobile/choose-character-controls.tsx
@@ -10,13 +10,13 @@ const ChooseCharacterControls = ({
   return (
     <>
       <div
-        className="absolute right-0 top-[50%] flex h-12 w-12 translate-x-1/2 items-center rounded-full bg-white"
+        className="absolute right-0 top-[50%] flex h-12 w-6 items-center overflow-hidden rounded-l-full bg-white"
         onClick={nextChamp}
       >
         <BiChevronRight className="h-7 w-auto fill-black" />
       </div>
       <div
-        className="absolute left-0 top-[50%] flex h-12 w-12 -translate-x-1/2 items-center justify-end rounded-full bg-white"
+        className="absolute left-0 top-[50%] flex h-12 w-6 items-center justify-end rounded-r-full bg-white"
         onClick={prevChamp}
       >
         <BiChevronLeft className="h-7 w-auto fill-black" />
