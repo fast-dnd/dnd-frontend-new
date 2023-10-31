1a4b9b32745f13a7b820f096e01a707e7729cf08
Milos Arsic <arsic.milos30@gmail.com>
2023-10-31 17:52:14 +0100

    storage added (#34)

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     domains: [
       "resource-images-bucket-fastdnd.s3.amazonaws.com",
       "dnd-images-bucket.s3.amazonaws.com",
-      "arweave.net",
+      "shdw-drive.genesysgo.net",
     ],
   },
 };
