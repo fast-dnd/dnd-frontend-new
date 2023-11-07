47b1200be47f00ec738c5331842db72f7f8856a2
pavle99 <mi18024@alas.matf.bg.ac.rs>
2023-11-07 14:52:57 +0100

    mobile profile tab toggle

---
+++ src/app/(authed)/profile/components/mobile/tab-toggle.tsx
@@ -0,0 +1,54 @@
+import { motion } from "framer-motion";
+
+import { cn } from "@/utils/style-utils";
+
+import { BaseTab } from "../../page";
+
+interface ITabToggleProps {
+  tabs: readonly BaseTab[];
+  activeTab: BaseTab;
+  setActiveTab: React.Dispatch<React.SetStateAction<BaseTab>>;
+}
+
+const TabToggle = ({ tabs, activeTab, setActiveTab }: ITabToggleProps) => {
+  return (
+    <div className={cn("flex justify-center")}>
+      <div className="pointer-events-auto relative flex w-full rounded-full border border-white/[8%] bg-black">
+        {tabs.map((tab) => (
+          <div
+            key={tab}
+            className={cn(
+              "z-10 mx-5 my-2.5 flex w-1/2 select-none items-center justify-center whitespace-nowrap text-sm uppercase opacity-50 transition-all",
+              tab === "MY PROFILE" && "gap-10",
+              tab === "MY COLLECTIONS" && "gap-2.5",
+              activeTab === tab && "opacity-100",
+            )}
+            onClick={() => {
+              setActiveTab(tab);
+            }}
+          >
+            {tab}
+          </div>
+        ))}
+
+        <div
+          className={cn(
+            "absolute inset-0 flex h-full w-full items-center justify-start p-0.5",
+            activeTab === "MY COLLECTIONS" && "justify-end",
+          )}
+        >
+          <motion.div
+            className={cn("h-9 w-1/2 rounded-full bg-primary")}
+            layout
+            transition={{
+              type: "spring",
+              stiffness: 200,
+              damping: 20,
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default TabToggle;
