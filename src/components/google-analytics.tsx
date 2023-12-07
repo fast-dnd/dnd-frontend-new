faf8302915c87f44b6513f365bf5dfc41065d78f
pavle99 <mi18024@alas.matf.bg.ac.rs>
2023-12-07 19:56:24 +0100

    add google tag manager

--- src/components/google-analytics.tsx
+++ src/components/google-analytics.tsx
@@ -13,34 +13,25 @@ const GoogleAnalytics = () => {
   useEffect(() => {
     const url = pathname + searchParams.toString();
 
-    window.gtag("config", env.NEXT_PUBLIC_GA_MEASUREMENT_ID, {
+    window.gtag("config", env.NEXT_PUBLIC_GTM_ID, {
       page_path: url,
     });
   }, [pathname, searchParams]);
 
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${env.NEXT_PUBLIC_GTM_ID}');
+        `}
+      </Script>
+      <noscript
         dangerouslySetInnerHTML={{
-          __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('consent', 'default', {
-                    'analytics_storage': 'denied'
-                });
-                
-                gtag('config', '${env.NEXT_PUBLIC_GA_MEASUREMENT_ID}', {
-                    page_path: window.location.pathname,
-                });
-                `,
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${env.NEXT_PUBLIC_GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
         }}
       />
     </>
