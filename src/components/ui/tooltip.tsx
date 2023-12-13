89409d59aa8ce3f68cd2ba38bfc8ab3703d10a14
pavle99 <mi18024@alas.matf.bg.ac.rs>
2023-12-13 10:50:36 +0100

    tooltip content inside portal

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -23,18 +23,20 @@ const TooltipContent = React.forwardRef<
   React.ElementRef<typeof TooltipPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
 >(({ className, sideOffset = 4, ...props }, ref) => (
-  <TooltipPrimitive.Content
-    ref={ref}
-    sideOffset={sideOffset}
-    className={cn(
-      "nozoom z-50 overflow-hidden rounded-md bg-black px-3 py-1.5 text-sm text-white shadow-sm shadow-white/20 animate-in fade-in-50 data-[side=bottom]:slide-in-from-top-1 data-[side=left]:slide-in-from-right-1 data-[side=right]:slide-in-from-left-1 data-[side=top]:slide-in-from-bottom-1",
-      className,
-    )}
-    {...props}
-  >
-    {props.children}
-    <TooltipArrow className="h-2 w-4 fill-black" />
-  </TooltipPrimitive.Content>
+  <TooltipPrimitive.Portal>
+    <TooltipPrimitive.Content
+      ref={ref}
+      sideOffset={sideOffset}
+      className={cn(
+        "nozoom z-50 overflow-hidden rounded-md bg-black px-3 py-1.5 text-sm text-white shadow-sm shadow-white/20 animate-in fade-in-50 data-[side=bottom]:slide-in-from-top-1 data-[side=left]:slide-in-from-right-1 data-[side=right]:slide-in-from-left-1 data-[side=top]:slide-in-from-bottom-1",
+        className,
+      )}
+      {...props}
+    >
+      {props.children}
+      <TooltipArrow className="nozoom h-2 w-4 fill-black" />
+    </TooltipPrimitive.Content>
+  </TooltipPrimitive.Portal>
 ));
 TooltipContent.displayName = TooltipPrimitive.Content.displayName;
 
