c1fb3e7e2960ec9d93afd7e71322d169071f4271
pavle99 <mi18024@alas.matf.bg.ac.rs>
2024-01-13 17:20:53 +0100

    communtiyid fix

--- src/hooks/helpers/use-community.tsx
+++ src/hooks/helpers/use-community.tsx
@@ -2,7 +2,7 @@ import { useReadLocalStorage } from "usehooks-ts";
 
 const useCommunity = () => {
   const defaultCommunityId = useReadLocalStorage<string>("defaultCommunityId");
-  const communityId = useReadLocalStorage<string>("communityId") ?? defaultCommunityId;
+  const communityId = useReadLocalStorage<string>("communityId");
   const isDefault = Boolean(
     defaultCommunityId && communityId && defaultCommunityId === communityId,
   );
