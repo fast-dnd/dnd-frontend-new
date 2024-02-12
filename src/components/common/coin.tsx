8937865d0803043343a1bbf62c2df8519523a426
pavle99 <mi18024@alas.matf.bg.ac.rs>
2024-02-13 00:16:53 +0100

    remove comments

--- src/components/common/coin.tsx
+++ src/components/common/coin.tsx
@@ -9,10 +9,6 @@ interface ICoinProps {
 
 const Coin = ({ silver = false }: ICoinProps) => {
   return (
-    // <div className="relative h-[30px] w-[30px] rounded-[5px] bg-rose-500 bg-opacity-10">
-    //   <img className="absolute left-[5px] top-[4.50px] h-5 w-5" src="/images/dm-coin.png" />
-    //   <div className="absolute left-[7px] top-[6.50px] h-4 w-4 rounded-[9px] bg-zinc-300 mix-blend-color" />
-    // </div>
     <Image
       src={`/images/${silver ? "silver" : "gold"}-coin.png`}
       alt="dm-coin"
--- src/utils/web3/rpc-url.ts
+++ src/utils/web3/rpc-url.ts
@@ -3,5 +3,3 @@ import { env } from "../env.mjs";
 const net = env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "mainnet" : "devnet";
 
 export const rpcNodeURL = `https://${net}.helius-rpc.com/?api-key=${env.NEXT_PUBLIC_WEB3_API_KEY}`;
-
-// https://devnet.helius-rpc.com/?api-key=e9221331-e94c-45fa-bcc4-09e6550ebcdb
