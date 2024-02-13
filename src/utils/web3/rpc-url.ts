c171b7fd576b755662a36b506d809d4a12aa8e4e
pavle99 <mi18024@alas.matf.bg.ac.rs>
2024-02-13 10:28:23 +0100

    switch to mainnet on dev

--- src/utils/web3/rpc-url.ts
+++ src/utils/web3/rpc-url.ts
@@ -1,5 +1,5 @@
 import { env } from "../env.mjs";
 
-const net = env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "mainnet" : "devnet";
+const net = env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "mainnet" : "mainnet"; //devnet
 
 export const rpcNodeURL = `https://${net}.helius-rpc.com/?api-key=${env.NEXT_PUBLIC_WEB3_API_KEY}`;
