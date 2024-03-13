8198ce1308ba1d1f0983adc18c91f02b9e629b46
Dusan Stanisavljevic <dusanstanisavljevic@Dusans-MacBook-Pro.local>
2024-03-13 21:06:21 +0100

    Remove some models

--- src/utils/ai-models.ts
+++ src/utils/ai-models.ts
@@ -16,14 +16,6 @@ export const aiModels = [
       "Wizard Uncle Bob, wielding the magic of OCT OCT MIXTRAL, crafts tales that enchant and mesmerize, taking listeners on journeys through mystical lands and magical adventures.",
   },
   {
-    name: "Farmer Bob (Llama)",
-    aiModel: "lama_farmer_bob",
-    imgUrl: "https://dnd-images-bucket.s3.amazonaws.com/lama+farmer.jpeg",
-    description: "Harvests stories from the heart",
-    longDescription:
-      "Lama Farmer Bob, with the gentle touch of OCT LLAMA INSTR 70B, cultivates stories that grow from the earth and blossom in the heart, narrating with a warmth that comforts the soul.",
-  },
-  {
     name: "Philosopher Bob (Nous Hermes)",
     aiModel: "philosopher_bob",
     imgUrl: "https://dnd-images-bucket.s3.amazonaws.com/philosopher.jpeg",
@@ -32,28 +24,12 @@ export const aiModels = [
       "Philosopher Bob, enlightened by OCT NOUS HERMES, delves deep into the essence of storytelling, offering insights and wisdom that provoke thought and ignite the imagination.",
   },
   {
-    name: "Hacker Bob (Gemini)",
-    aiModel: "hacker_bob",
-    imgUrl: "https://dnd-images-bucket.s3.amazonaws.com/hacker.jpeg",
-    description: "Navigates narratives through the digital age",
-    longDescription:
-      "Hacker Bob, powered by OR GEMINI, hacks into the heart of tales, navigating through narratives with a digital savvy that unveils the unseen connections in the web of stories.",
-  },
-  {
     name: "Zookeeper Bob (MythoMax)",
     aiModel: "zookeeper_bob",
     imgUrl: "https://dnd-images-bucket.s3.amazonaws.com/zookeeper.jpeg",
     description: "Cares for stories like wild creatures(supports NSFW)",
     longDescription:
       "Zookeeper Bob, guided by OR MYTHO MAX, nurtures tales with the care of a devoted zookeeper, ensuring each story grows wild and free, roaming the vast landscapes of imagination.",
-  },
-  {
-    name: "Safari Guide Bob (MythoMax Nitro)",
-    aiModel: "safari_guide_bob",
-    imgUrl: "https://dnd-images-bucket.s3.amazonaws.com/safari+guide.jpeg",
-    description: "Leads expeditions into untamed narratives(supports NSFW)",
-    longDescription:
-      "Safari Guide Bob, with the expertise of OR MYTHO MAX NITRO, leads daring expeditions into the heart of untamed narratives, tracking the footprints of stories seldom told.",
   },
   {
     name: "Adventurer Bob (Izlv)",
