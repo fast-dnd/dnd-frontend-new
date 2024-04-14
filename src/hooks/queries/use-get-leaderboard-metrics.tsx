fc7cee116ee0dc3607ebf8a62c3bd113c3e1ab0e
pavle99 <mi18024@alas.matf.bg.ac.rs>
2024-04-14 15:05:14 +0200

    fix leaderboard metrics when no token

--- src/hooks/queries/use-get-leaderboard-metrics.tsx
+++ src/hooks/queries/use-get-leaderboard-metrics.tsx
@@ -2,15 +2,17 @@ import { useQuery } from "@tanstack/react-query";
 
 import accountService, { accountKey } from "@/services/account-service";
 
+import useAuth from "../helpers/use-auth";
 import useCommunity from "../helpers/use-community";
 
 const useGetLeaderboardMetrics = () => {
   const { communityId } = useCommunity();
+  const { loggedIn } = useAuth();
 
   return useQuery({
     queryKey: [accountKey, communityId, "rating"],
     queryFn: () => accountService.getLeaderboardMetrics(communityId ?? ""),
-    enabled: !!communityId,
+    enabled: !!communityId && loggedIn,
   });
 };
 
