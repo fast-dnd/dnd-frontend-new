9f092586ef1d8df0323c2af41e0780a1aa0dc750
dusanstanis <dusan.stanisavljevicposlovni98@gmail.com>
2024-08-05 21:13:40 +0200

    Init ora network integration

--- src/components/common/game-history.tsx
+++ src/components/common/game-history.tsx
@@ -1,4 +1,4 @@
-"use client";
+import React from "react";
 
 import RoomItem from "@/components/common/room-item";
 import QuillIcon from "@/components/icons/quill-icon";
@@ -8,7 +8,9 @@ import Spinner from "@/components/ui/spinner";
 import useIntersectionObserver from "@/hooks/helpers/use-intersection-observer";
 import useGetRoomHistory from "@/hooks/queries/use-get-room-history";
 
-const GameHistory = ({ showFull = false }: { showFull?: boolean }) => {
+import OraNetworkModal from "./ora-network-modal";
+
+const GameHistory = ({ showFull = false }) => {
   const {
     data: roomsData,
     hasNextPage,
@@ -25,61 +27,33 @@ const GameHistory = ({ showFull = false }: { showFull?: boolean }) => {
   });
 
   if (isLoading) {
-    return (
-      <div className="no-scrollbar flex flex-1 flex-col gap-4 overflow-y-auto">
-        {showFull && <div className="mb-3 h-5 w-32 animate-pulse rounded-xl bg-gray-600" />}
-        <Skeleton small amount={2} />
-      </div>
-    );
+    return <Skeleton small amount={2} />;
   }
 
   if (isError) {
-    return (
-      <div className="flex size-full items-center justify-center">
-        <div className="text-5xl text-white">Something went wrong</div>
-      </div>
-    );
+    return <div>Something went wrong</div>;
   }
 
   const content = showFull
     ? roomsData.pages.map((page) =>
-        page.rooms.map((room, i) => {
-          if (page.rooms.length === i + 1) {
-            return <RoomItem key={room.conversationId} room={room} ref={lastRoomItemRef} />;
-          }
-          return <RoomItem key={room.conversationId} room={room} />;
-        }),
+        page.rooms.map((room, i) => (
+          <RoomItem key={room.conversationId} room={room} ref={lastRoomItemRef} />
+        )),
       )
     : roomsData.pages[0].rooms
         .slice(0, 3)
         .map((room) => <RoomItem key={room.conversationId} room={room} />);
 
   return (
-    <div className="flex min-h-0 w-full min-w-0 flex-1 flex-col gap-6">
-      {showFull && <p className="uppercase">Games played: {roomsData?.pages[0].total ?? "--"}</p>}
-
-      <div className="flex size-full flex-col gap-4 overflow-y-auto pb-2 pr-4">
-        {roomsData.pages[0].rooms.length === 0 ? (
-          <ZeroGames />
-        ) : (
+    <div className="flex flex-1 flex-col gap-6">
+      {showFull && <p>Games played: {roomsData?.pages[0].total ?? "--"}</p>}
+      <div className="flex flex-1 flex-col gap-4 overflow-y-auto">
+        {content}
+        {isFetchingNextPage && <Spinner />}
+        {!showFull && (
           <>
-            {content}
-            {isFetchingNextPage && (
-              <div className="flex h-10 justify-center">
-                <Spinner className="m-0 size-8" />
-              </div>
-            )}
-            {!showFull && (
-              <div className="mt-auto">
-                <Button
-                  className="whitespace-nowrap"
-                  href="/profile?activeTab=GAME HISTORY"
-                  variant="outline"
-                >
-                  SHOW ENTIRE GAME HISTORY
-                </Button>
-              </div>
-            )}
+            <Button href="/profile?activeTab=GAME HISTORY">SHOW ENTIRE GAME HISTORY</Button>
+            <OraNetworkModal />
           </>
         )}
       </div>
@@ -89,19 +63,9 @@ const GameHistory = ({ showFull = false }: { showFull?: boolean }) => {
 
 export default GameHistory;
 
-const ZeroGames = () => {
-  return (
-    <div className="flex w-full items-center justify-center">
-      <div className="flex h-full w-[490px] flex-col items-center justify-start gap-5 p-5 lg:gap-8 lg:p-8">
-        <QuillIcon />
-        <p className="text-center text-lg font-semibold uppercase leading-7 tracking-[3.30px] lg:text-xl">
-          No Games in Your History... FOR NOW
-        </p>
-        <p className="text-center text-sm font-normal leading-7 tracking-widest text-white/50 lg:text-base">
-          Your tale is yet to be written, adventurer. Set forth and embark on your first epic
-          journey.
-        </p>
-      </div>
-    </div>
-  );
-};
+const ZeroGames = () => (
+  <div>
+    <QuillIcon />
+    <p>No Games in Your History... FOR NOW</p>
+  </div>
+);
---
+++ src/components/common/ora-network-modal/index.tsx
@@ -0,0 +1,320 @@
+/* eslint-disable tailwindcss/migration-from-tailwind-2 */
+/* eslint-disable react/jsx-key */
+/* eslint-disable @next/next/no-img-element */
+/* eslint-disable tailwindcss/no-custom-classname */
+import { useState } from "react";
+import { Gift } from "@phosphor-icons/react";
+import { DialogClose } from "@radix-ui/react-dialog";
+import { AiOutlineClose } from "react-icons/ai";
+import Web3 from "web3";
+
+import { Button } from "@/components/ui/button";
+import { jibril } from "@/utils/fonts";
+
+import { Dialog, DialogContent, DialogTrigger } from "../../ui/dialog";
+
+const OraNetworkModal = () => {
+  const [selectedNetwork, setSelectedNetwork] = useState<NetworkName | "">("");
+
+  const handleNetworkSelection = (network: NetworkName) => {
+    setSelectedNetwork(network);
+  };
+  return (
+    <Dialog>
+      <DialogTrigger asChild className="max-lg:hidden">
+        <button className="flex cursor-pointer items-center justify-center rounded-full border border-white/20 bg-black p-4 transition-all duration-200 hover:bg-[#1B1B1B]">
+          <Gift size={32} />
+        </button>
+      </DialogTrigger>
+      <DialogTrigger asChild className="lg:hidden">
+        <Button className="gap-4 py-4" variant="sidebar">
+          <Gift className="size-5 shrink-0 fill-white" />
+          <p className="flex-1 text-center">claim reward</p>
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="z-[100] flex flex-col gap-12 bg-black p-4 max-lg:size-full max-lg:max-w-full max-lg:rounded-none max-lg:bg-dark-900 lg:p-8">
+        <div className="flex flex-col gap-4">
+          <div className="flex justify-end lg:hidden">
+            <DialogClose>
+              <AiOutlineClose />
+            </DialogClose>
+          </div>
+          <div className="flex items-center justify-center gap-4">
+            <div className="size-2 shrink-0 rotate-45 bg-primary" />
+            <p
+              className="mt-1 text-lg uppercase tracking-widest lg:text-2xl lg:tracking-[6.4px]"
+              style={jibril.style}
+            >
+              Participate in ORA network Leaderboard
+            </p>
+            <div className="size-2 shrink-0 rotate-45 bg-primary" />
+          </div>
+          <br />
+          <br />
+          <p className="ml-2 text-center font-light lg:text-xl lg:tracking-[1.5px]">
+            By selecting network and executing transaction given transcript will be evaulated by AI
+            Judge and depending on its rating you will climb ORA network leaderboard !
+          </p>
+          <div className="mt-4 flex flex-wrap justify-center gap-4">
+            {Object.entries(networks).map(([name, id]) => (
+              <NetworkButton
+                networkName={name as NetworkName}
+                onClick={() => handleNetworkSelection(name as NetworkName)}
+                networkLogos={networkLogos}
+                isSelected={selectedNetwork === name}
+              />
+            ))}
+          </div>
+          <Button
+            onClick={() => handleOraNetworkClick(selectedNetwork)}
+            className="hover:bg-primary-dark bg-primary"
+            disabled={!selectedNetwork}
+          >
+            Pay to Compete
+          </Button>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+type NetworkName =
+  | "Ethereum"
+  // | "Ethereum Sepolia"
+  | "Optimism"
+  // | "Optimism Sepolia"
+  | "Arbitrum"
+  // | "Arbitrum Sepolia Testnet"
+  | "Manta"
+  // | "Manta Sepolia Testnet"
+  | "Linea"
+  | "Base"
+  | "Polygon"
+  | "Mantle";
+
+const networkLogos: { [key in NetworkName]: string } = {
+  Ethereum: "/images/logos/ethereum-eth-logo.png",
+  // "Ethereum Sepolia": "/images/logos/ethereum-eth-logo.png",
+  Optimism: "/images/logos/optimism-ethereum-op-logo.png",
+  // "Optimism Sepolia": "/images/logos/optimism-ethereum-op-logo.png",
+  Arbitrum: "/images/logos/arbitrum-arb-logo.png",
+  // "Arbitrum Sepolia Testnet": "/images/logos/ethereum-eth-logo.png",
+  Manta: "/images/logos/manta-logo.png",
+  // "Manta Sepolia Testnet": "/images/logos/ethereum-eth-logo.png",
+  Linea: "/images/logos/linea-logo.png",
+  Base: "/images/logos/base-network-logo.png",
+  Polygon: "/images/logos/polygon-logo.png",
+  Mantle: "/images/logos/mantle-logo.png",
+};
+const networks: Record<NetworkName, string> = {
+  Ethereum: "0x1",
+  // "Ethereum Sepolia": "0xaa36a7",
+  Optimism: "0xa",
+  // "Optimism Sepolia": "0xaa37dc",
+  Arbitrum: "0xa4b1",
+  // "Arbitrum Sepolia Testnet": "0x66eee",
+  Manta: "0xa9",
+  // "Manta Sepolia Testnet": "0x34816e",
+  Linea: "0xe708",
+  Base: "0x2105",
+  Polygon: "0x89",
+  Mantle: "0x1388",
+};
+
+const contractAddresses: { [key in NetworkName]: string } = {
+  Ethereum: "0x61423153f111BCFB28dd264aBA8d9b5C452228D2",
+  // "Ethereum Sepolia": "0x696c83111a49eBb94267ecf4DDF6E220D5A80129",
+  Optimism: "0xBC24514E541d5CBAAC1DD155187A171a593e5CF6",
+  // "Optimism Sepolia": "0xf6919ebb1bFdD282c4edc386bFE3Dea1a1D8AC16",
+  Arbitrum: "0xC3287BDEF03b925A7C7f54791EDADCD88e632CcD",
+  // "Arbitrum Sepolia Testnet": "0xBC24514E541d5CBAAC1DD155187A171a593e5CF6",
+  Manta: "0x523622DfEd0243B0DF80CC9275764B0f432D33E3",
+  // "Manta Sepolia Testnet": "0x3bfD1Cc919bfeC7795b600E764aDa001b58f122a",
+  Linea: "0xb880D47D3894D99157B52A7F869aB3B1E2D4349d",
+  Base: "0xC3287BDEF03b925A7C7f54791EDADCD88e632CcD",
+  Polygon: "0xC3287BDEF03b925A7C7f54791EDADCD88e632CcD",
+  Mantle: "0xC3287BDEF03b925A7C7f54791EDADCD88e632CcD",
+};
+
+interface NetworkButtonProps {
+  networkName: NetworkName;
+  onClick: (networkName: NetworkName) => void;
+  networkLogos: { [key: string]: string };
+  isSelected: boolean;
+}
+
+const NetworkButton: React.FC<NetworkButtonProps> = ({ networkName, onClick, isSelected }) => {
+  const selectedClass = isSelected ? "ring-4 ring-primary-light" : "";
+  return (
+    <div className={`network-button-container mt-4 flex justify-center ${selectedClass}`}>
+      <button
+        onClick={() => onClick(networkName)}
+        className={`network-button hover:bg-primary-dark transform transition duration-300 ease-in-out hover:scale-110 ${
+          isSelected ? "bg-selected" : "bg-normal"
+        }`}
+      >
+        <img
+          src={networkLogos[networkName]}
+          alt={`${networkName} logo`}
+          style={{ width: "100px", height: "100px", objectFit: "contain" }}
+          className="network-logo"
+        />
+        <span className="network-name">{networkName}</span>
+      </button>
+    </div>
+  );
+};
+
+const handleOraNetworkClick = async (selectedNetwork: string) => {
+  if (typeof window.ethereum !== "undefined") {
+    try {
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+      const accounts = await window.ethereum.request({ method: "eth_accounts" });
+      const selectedAccount = accounts[0];
+
+      const networkChoice = selectedNetwork as NetworkName;
+
+      if (networkChoice && networks[networkChoice]) {
+        await window.ethereum.request({
+          method: "wallet_switchEthereumChain",
+          params: [{ chainId: networks[networkChoice] }],
+        });
+
+        const web3 = new Web3(window.ethereum);
+        const contractAddress = contractAddresses[networkChoice];
+        console.log("contractAddress", contractAddress);
+        console.log("oraAbi", oraAbi);
+        const contract = new web3.eth.Contract(oraAbi, contractAddress);
+
+        const modelId = 11;
+        const prompt = "Hello, what is your name?";
+        console.log("contract.methods", contract.methods);
+        try {
+          const response = await contract.methods.calculateAIResult(modelId, prompt).send({
+            from: selectedAccount,
+          });
+
+          console.log("Response:", response);
+        } catch (methodError) {
+          console.error("Error calling calculateAIResult:", methodError);
+        }
+      } else {
+        alert("Invalid network selection or network not supported.");
+      }
+    } catch (error) {
+      console.error("An error occurred with MetaMask:", error);
+    }
+  } else {
+    alert("MetaMask is not installed. Please install it to proceed.");
+  }
+};
+
+const oraAbi: any[] = [
+  {
+    inputs: [{ internalType: "contract IAIOracle", name: "_aiOracle", type: "address" }],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      { internalType: "contract IAIOracle", name: "expected", type: "address" },
+      { internalType: "contract IAIOracle", name: "found", type: "address" },
+    ],
+    name: "UnauthorizedCallbackSource",
+    type: "error",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: false, internalType: "uint256", name: "requestId", type: "uint256" },
+      { indexed: false, internalType: "address", name: "sender", type: "address" },
+      { indexed: false, internalType: "uint256", name: "modelId", type: "uint256" },
+      { indexed: false, internalType: "string", name: "prompt", type: "string" },
+    ],
+    name: "promptRequest",
+    type: "event",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      { indexed: false, internalType: "uint256", name: "requestId", type: "uint256" },
+      { indexed: false, internalType: "string", name: "output", type: "string" },
+      { indexed: false, internalType: "bytes", name: "callbackData", type: "bytes" },
+    ],
+    name: "promptsUpdated",
+    type: "event",
+  },
+  {
+    inputs: [],
+    name: "aiOracle",
+    outputs: [{ internalType: "contract IAIOracle", name: "", type: "address" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "requestId", type: "uint256" },
+      { internalType: "bytes", name: "output", type: "bytes" },
+      { internalType: "bytes", name: "callbackData", type: "bytes" },
+    ],
+    name: "aiOracleCallback",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "modelId", type: "uint256" },
+      { internalType: "string", name: "prompt", type: "string" },
+    ],
+    name: "calculateAIResult",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    name: "callbackGasLimit",
+    outputs: [{ internalType: "uint64", name: "", type: "uint64" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "modelId", type: "uint256" }],
+    name: "estimateFee",
+    outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "requestId", type: "uint256" }],
+    name: "isFinalized",
+    outputs: [{ internalType: "bool", name: "", type: "bool" }],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+    name: "requests",
+    outputs: [
+      { internalType: "address", name: "sender", type: "address" },
+      { internalType: "uint256", name: "modelId", type: "uint256" },
+      { internalType: "bytes", name: "input", type: "bytes" },
+      { internalType: "bytes", name: "output", type: "bytes" },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      { internalType: "uint256", name: "modelId", type: "uint256" },
+      { internalType: "uint64", name: "gasLimit", type: "uint64" },
+    ],
+    name: "setCallbackGasLimit",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+export default OraNetworkModal;
