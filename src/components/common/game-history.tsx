615d6b738d5116ccb623568649b9a42a61a6b642
dusanstanis <dusan.stanisavljevicposlovni98@gmail.com>
2024-08-07 08:50:11 +0200

    Revert game-history design changes

--- src/components/common/game-history.tsx
+++ src/components/common/game-history.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+"use client";
 
 import RoomItem from "@/components/common/room-item";
 import QuillIcon from "@/components/icons/quill-icon";
@@ -8,7 +8,7 @@ import Spinner from "@/components/ui/spinner";
 import useIntersectionObserver from "@/hooks/helpers/use-intersection-observer";
 import useGetRoomHistory from "@/hooks/queries/use-get-room-history";
 
-const GameHistory = ({ showFull = false }) => {
+const GameHistory = ({ showFull = false }: { showFull?: boolean }) => {
   const {
     data: roomsData,
     hasNextPage,
@@ -25,32 +25,61 @@ const GameHistory = ({ showFull = false }) => {
   });
 
   if (isLoading) {
-    return <Skeleton small amount={2} />;
+    return (
+      <div className="no-scrollbar flex flex-1 flex-col gap-4 overflow-y-auto">
+        {showFull && <div className="mb-3 h-5 w-32 animate-pulse rounded-xl bg-gray-600" />}
+        <Skeleton small amount={2} />
+      </div>
+    );
   }
 
   if (isError) {
-    return <div>Something went wrong</div>;
+    return (
+      <div className="flex size-full items-center justify-center">
+        <div className="text-5xl text-white">Something went wrong</div>
+      </div>
+    );
   }
 
   const content = showFull
     ? roomsData.pages.map((page) =>
-        page.rooms.map((room, i) => (
-          <RoomItem key={room.conversationId} room={room} ref={lastRoomItemRef} />
-        )),
+        page.rooms.map((room, i) => {
+          if (page.rooms.length === i + 1) {
+            return <RoomItem key={room.conversationId} room={room} ref={lastRoomItemRef} />;
+          }
+          return <RoomItem key={room.conversationId} room={room} />;
+        }),
       )
     : roomsData.pages[0].rooms
         .slice(0, 3)
         .map((room) => <RoomItem key={room.conversationId} room={room} />);
 
   return (
-    <div className="flex flex-1 flex-col gap-6">
-      {showFull && <p>Games played: {roomsData?.pages[0].total ?? "--"}</p>}
-      <div className="flex flex-1 flex-col gap-4 overflow-y-auto">
-        {content}
-        {isFetchingNextPage && <Spinner />}
-        {!showFull && (
+    <div className="flex min-h-0 w-full min-w-0 flex-1 flex-col gap-6">
+      {showFull && <p className="uppercase">Games played: {roomsData?.pages[0].total ?? "--"}</p>}
+
+      <div className="flex size-full flex-col gap-4 overflow-y-auto pb-2 pr-4">
+        {roomsData.pages[0].rooms.length === 0 ? (
+          <ZeroGames />
+        ) : (
           <>
-            <Button href="/profile?activeTab=GAME HISTORY">SHOW ENTIRE GAME HISTORY</Button>
+            {content}
+            {isFetchingNextPage && (
+              <div className="flex h-10 justify-center">
+                <Spinner className="m-0 size-8" />
+              </div>
+            )}
+            {!showFull && (
+              <div className="mt-auto">
+                <Button
+                  className="whitespace-nowrap"
+                  href="/profile?activeTab=GAME HISTORY"
+                  variant="outline"
+                >
+                  SHOW ENTIRE GAME HISTORY
+                </Button>
+              </div>
+            )}
           </>
         )}
       </div>
@@ -60,9 +89,19 @@ const GameHistory = ({ showFull = false }) => {
 
 export default GameHistory;
 
-const ZeroGames = () => (
-  <div>
-    <QuillIcon />
-    <p>No Games in Your History... FOR NOW</p>
-  </div>
-);
+const ZeroGames = () => {
+  return (
+    <div className="flex w-full items-center justify-center">
+      <div className="flex h-full w-[490px] flex-col items-center justify-start gap-5 p-5 lg:gap-8 lg:p-8">
+        <QuillIcon />
+        <p className="text-center text-lg font-semibold uppercase leading-7 tracking-[3.30px] lg:text-xl">
+          No Games in Your History... FOR NOW
+        </p>
+        <p className="text-center text-sm font-normal leading-7 tracking-widest text-white/50 lg:text-base">
+          Your tale is yet to be written, adventurer. Set forth and embark on your first epic
+          journey.
+        </p>
+      </div>
+    </div>
+  );
+};
