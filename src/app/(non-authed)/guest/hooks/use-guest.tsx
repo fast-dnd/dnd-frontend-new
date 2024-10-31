994ac33232a41622c6e7bc312d5a95e3850ae6fe
dusanstanis <dusan.stanisavljevicposlovni98@gmail.com>
2024-10-31 08:43:45 +0100

    Always send guest data if it exists

--- src/app/(non-authed)/guest/hooks/use-guest.tsx
+++ src/app/(non-authed)/guest/hooks/use-guest.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useLocalStorage, useReadLocalStorage } from "usehooks-ts";
+import { useLocalStorage } from "usehooks-ts";
 import { v4 as uuidv4 } from "uuid";
 
 export interface GuestData {
@@ -11,34 +11,24 @@ export interface GuestData {
 const GUEST_KEY = "guest";
 
 const useGuest = (): GuestData | null => {
-  const jwtToken = useReadLocalStorage<string | null>("jwtToken");
   const [guestData, setGuestData] = useLocalStorage<GuestData | null>(GUEST_KEY, null);
 
   useEffect(() => {
-    if (!jwtToken) {
-      // User is not authenticated
-      const now = new Date();
+    const now = new Date();
 
-      if (guestData && guestData.createdAt) {
-        const createdAt = new Date(guestData.createdAt);
-        const timeDifference = now.getTime() - createdAt.getTime();
-        const hoursDifference = timeDifference / (1000 * 3600);
+    if (guestData && guestData.createdAt) {
+      const createdAt = new Date(guestData.createdAt);
+      const timeDifference = now.getTime() - createdAt.getTime();
+      const hoursDifference = timeDifference / (1000 * 3600);
 
-        if (hoursDifference >= 24) {
-          // More than 24 hours have passed; generate new guest data
-          generateNewGuestData();
-        }
-      } else {
-        // No guest data; generate new guest data
+      if (hoursDifference >= 24) {
+        // More than 24 hours have passed; generate new guest data
         generateNewGuestData();
       }
     } else {
-      // User is authenticated
-      // Optionally, remove guest data from local storage
-      setGuestData(null);
+      generateNewGuestData();
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [jwtToken]);
+  }, [guestData, setGuestData]);
 
   const generateNewGuestData = () => {
     const newGuestId = uuidv4();
