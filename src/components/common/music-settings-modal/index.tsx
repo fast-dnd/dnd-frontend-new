da0621bb9768d5cdc52fda2af0ab5a7cd01195d5
dusanstanis <dusan.stanisavljevicposlovni98@gmail.com>
2024-12-24 16:18:29 +0100

    init fix mobile audio settings

--- src/components/common/music-settings-modal/index.tsx
+++ src/components/common/music-settings-modal/index.tsx
@@ -8,8 +8,6 @@ import { Slider } from "@/components/ui/slider";
 import { Dialog, DialogContent, DialogTrigger } from "../../ui/dialog";
 import ToggleSwitch from "../toggle-switch";
 
-let audio: HTMLAudioElement | null = null;
-
 const soundtracks = [
   "/sounds/background-music-1.mp3",
   "/sounds/background-music-2.mp3",
@@ -22,13 +20,13 @@ const MusicSettingsModal = () => {
   const [soundVolume, setSoundVolume] = useState(20);
   const [currentTrackIndex, setCurrentTrackIndex] = useState(() =>
     Math.floor(Math.random() * soundtracks.length),
-  ); // Start with a random track
+  );
   const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
 
   useEffect(() => {
     let newAudio: HTMLAudioElement | null = new Audio(soundtracks[currentTrackIndex]);
     newAudio.loop = false;
-    newAudio.volume = musicVolume / 100; // Set initial volume
+    newAudio.volume = musicVolume / 100;
 
     if (isMusicPlaying) {
       newAudio.play();
@@ -39,8 +37,6 @@ const MusicSettingsModal = () => {
     };
 
     newAudio.addEventListener("ended", handleTrackEnd);
-
-    // Store the audio object in state
     setAudio(newAudio);
 
     return () => {
@@ -50,9 +46,8 @@ const MusicSettingsModal = () => {
         newAudio = null;
       }
     };
-  }, [isMusicPlaying, currentTrackIndex]);
+  }, [isMusicPlaying, currentTrackIndex, musicVolume]);
 
-  // Dynamically update the volume without resetting the track or audio state
   useEffect(() => {
     if (audio) {
       audio.volume = musicVolume / 100;
@@ -61,19 +56,22 @@ const MusicSettingsModal = () => {
 
   return (
     <Dialog>
-      <DialogTrigger asChild className="max-lg:hidden">
-        <button className="flex items-center gap-2 hover:opacity-70">
-          {isMusicPlaying ? (
-            <MusicNote className="size-7" color="green" />
-          ) : (
-            <MusicNote className="size-7" color="white" />
-          )}
+      <DialogTrigger asChild>
+        <button
+          className="rounded-lg p-2 focus:outline-none focus:ring-2 focus:ring-white/20"
+          aria-label="Music settings"
+        >
+          <MusicNote
+            className={`
+              ${isMusicPlaying ? "text-green-500" : "text-white"} h-4 w-4 lg:h-8 lg:w-8
+            `}
+          />
         </button>
       </DialogTrigger>
-      <DialogContent className="flex h-auto max-h-full w-full max-w-md flex-col gap-12 bg-black p-4 max-lg:rounded-none max-lg:bg-dark-900 lg:h-auto lg:max-w-lg lg:p-8">
+      <DialogContent className="z-[100] flex flex-col gap-12 bg-black p-4 max-lg:size-full max-lg:max-h-[80vh] max-lg:max-w-full max-lg:overflow-y-auto max-lg:rounded-none max-lg:bg-dark-900 lg:p-8">
         <div className="flex flex-col gap-4">
-          <div className="flex justify-end lg:hidden">
-            <DialogClose>
+          <div className="flex justify-end">
+            <DialogClose className="rounded-lg p-2 hover:bg-white/10">
               <AiOutlineClose />
             </DialogClose>
           </div>
@@ -83,7 +81,7 @@ const MusicSettingsModal = () => {
             </p>
             <ToggleSwitch
               on={isMusicPlaying}
-              setOn={(checked) => setIsMusicPlaying(checked)}
+              setOn={setIsMusicPlaying}
               items={[
                 { text: "Off", icon: <MusicNote size={14} /> },
                 { text: "On", icon: <MusicNote size={14} /> },
@@ -95,7 +93,7 @@ const MusicSettingsModal = () => {
                 max={100}
                 label="Music volume"
                 value={[musicVolume]}
-                onValueChange={(newValue: any) => setMusicVolume(newValue[0])}
+                onValueChange={(newValue) => setMusicVolume(newValue[0])}
                 aria-label="Music volume"
               />
               <Slider
@@ -103,7 +101,7 @@ const MusicSettingsModal = () => {
                 max={100}
                 label="Sound volume"
                 value={[soundVolume]}
-                onValueChange={(newValue: any) => setSoundVolume(newValue[0])}
+                onValueChange={(newValue) => setSoundVolume(newValue[0])}
                 aria-label="Sound volume"
               />
             </div>
